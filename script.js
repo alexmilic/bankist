70ce73b49396a13e82417ae06b5f67c48f8d4762
Aleksandar Milic <alexmilic01@gmail.com>
2021-05-22 18:47:20 +0200

    Add fuction for incomes, out, interests

--- script.js
+++ script.js
@@ -61,16 +61,66 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function(movements) {
+
+    containerMovements.innerHTML = '';
+
+    movements.forEach(function(mov, i) {
+        const type = mov < 0 ? 'deposit' : 'withdrawal';
+        const html = `<div class="movements__row">
+        <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+        <div class="movements__value">${mov}€</div>
+      </div>`;
+
+      containerMovements.insertAdjacentHTML('afterbegin', html);
+
+    });
+}
+
+displayMovements(account1.movements);
+
+const calcDisplayBalance = function(movements) {
+    const balance = movements.reduce((acc, move) => acc + move);
+    labelBalance.textContent = `${balance}€`;
+}
+
+const calcDisplaySummary = function(movements) {
+    const incomes = movements
+        .filter(mov => mov > 0)
+        .reduce((acc, mov) => acc + mov, 0);
+    labelSumIn.textContent = `${incomes}€`;
+    
+    const out = movements
+        .filter(mov => mov < 0)
+        .reduce((acc, mov) => acc + mov, 0);
+    labelSumOut.textContent = `${Math.abs(out)}€`;
+
+    const interest = movements
+        .filter(mov =>  mov > 0)
+        .map(deposit => deposit * 1.2 / 100)
+        .filter((int, i, arr) => {
+            console.log(arr);
+            return int >= 1;
+        })
+        .reduce((acc, int) => acc + int, 0);
+    labelSumInterest.textContent = `${interest}€`;
+}
+
+calcDisplaySummary(account1.movements);
+
+
+const createUsernames = function(accs) {
+    accs.forEach(function(acc) {
+        acc.username = acc.owner
+            .toLowerCase()
+            .split(' ')
+            .map(name => name[0])
+            .join('');
+    });
+}
+
+createUsernames(accounts);
+calcDisplayBalance(account1.movements);
+
 /////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
 /////////////////////////////////////////////////
